aab7a4c77742935d999f5d13b3b16f4c0d4ad320
Scott Ho <sscott.ho@gmail.com>
2023-11-22 03:46:30 -0700

    Refactor menu, add 4 action buttons and an interact button

--- level.gd
+++ level.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var moveService: MoveService = $MoveService
 @onready var menuService: MenuService = $MenuService
 @onready var turnService: TurnService = $TurnService
+@onready var tileMap: MainTileMap = $TileMap
 @onready var highlightMap: HighlightMap = $HighlightMap
 var current_turn_id: int = -1
 var state: State = State.new()
@@ -31,7 +32,7 @@ func importTestData():
     ally1.location = Vector2i(1,0)
     var sprite1 = load("res://character_body_2d.tscn").instantiate()
     add_child(sprite1)
-    sprite1.global_position = moveService.pointToGlobal(ally1.location)
+    sprite1.global_position = tileMap.pointToGlobal(ally1.location)
     ally1.sprite = sprite1
     var id1 = state.addAlly(ally1)
     sprite1.setLabel(str(id1))
@@ -42,7 +43,7 @@ func importTestData():
     ally2.location = Vector2i(1,1)
     var sprite2 = load("res://character_body_2d.tscn").instantiate()
     add_child(sprite2)
-    sprite2.global_position = moveService.pointToGlobal(ally2.location)
+    sprite2.global_position = tileMap.pointToGlobal(ally2.location)
     ally2.sprite = sprite2
     var id2 = state.addAlly(ally2)
     sprite2.setLabel(str(id2))
@@ -53,7 +54,7 @@ func importTestData():
     ally3.location = Vector2i(1,2)
     var sprite3 = load("res://character_body_2d.tscn").instantiate()
     add_child(sprite3)
-    sprite3.global_position = moveService.pointToGlobal(ally3.location)
+    sprite3.global_position = tileMap.pointToGlobal(ally3.location)
     ally3.sprite = sprite3
     var id3 = state.addAlly(ally3)
     sprite3.setLabel(str(id3))
@@ -64,7 +65,7 @@ func importTestData():
     ally4.location = Vector2i(1,3)
     var sprite4  = load("res://character_body_2d.tscn").instantiate()
     add_child(sprite4)
-    sprite4.global_position = moveService.pointToGlobal(ally4.location)
+    sprite4.global_position = tileMap.pointToGlobal(ally4.location)
     ally4.sprite = sprite4
     var id4 = state.addAlly(ally4)
     sprite4.setLabel(str(id4))
--- menu.gd
+++ menu.gd
@@ -3,39 +3,46 @@ class_name MenuService extends Node2D
 var _state: State
 signal moveActionInitiate
 signal nextTurnActionInitiate
-@onready var moveButton = $Control/Button
-@onready var label = $Control/Label
+signal interactActionInitiate
+signal action1Initiate
+signal action2Initiate
+signal action3Initiate
+signal action4Initiate
+@onready var moveButton: Button = $Control/MoveButton
+@onready var nextTurnButton: Button = $Control/TurnButton
+@onready var interactButton: Button = $Control/InteractButton
+@onready var action1Button: Button = $Control/ActionTree/Action1Button
+@onready var action2Button: Button = $Control/ActionTree/Action2Button
+@onready var action3Button: Button = $Control/ActionTree/Action3Button
+@onready var action4Button: Button = $Control/ActionTree/Action4Button
+@onready var numMovesLabel: Label = $Control/NumMovesLabel
+@onready var currentTurnLabel: Label = $Control/TurnLabelsContainer/CurrentTurnLabel
+@onready var futureTurnsLabel: Label = $Control/TurnLabelsContainer/FutureTurnsLabel
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    moveButton.button_down.connect(emitMoveButton)
-    $Control/Button2.button_down.connect(emitNextTurn)
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
+    moveButton.button_down.connect(func(): moveActionInitiate.emit())
+    nextTurnButton.button_down.connect(func(): nextTurnActionInitiate.emit())
+    interactButton.button_down.connect(func(): interactActionInitiate.emit())
+    action1Button.button_down.connect(func(): action1Initiate.emit())
+    action2Button.button_down.connect(func(): action2Initiate.emit())
+    action3Button.button_down.connect(func(): action3Initiate.emit())
+    action4Button.button_down.connect(func(): action4Initiate.emit())
+    return
 
 func setState(state: State):
     _state = state
     return
 
-func emitMoveButton():
-    moveActionInitiate.emit()
-    return
-
-func emitNextTurn():
-    nextTurnActionInitiate.emit()
-    return
-
 func setMoveNum(i: int):
-    label.text = str(i)
+    numMovesLabel.text = str(i)
     return
 
 func showTurns(turns: Array[int]):
-    $Control/Label2.text = str(turns)
+    futureTurnsLabel.text = str(turns)
     return
 
 func showCurrentTurn(turn: int):
-    $Control/Label3.text = str(turn)
+    currentTurnLabel.text = str(turn)
     return
