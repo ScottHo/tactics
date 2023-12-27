1178e1f9e4083fdc047e3fce73566b0ef6eea643
Scott Ho <sscott.ho@gmail.com>
2023-12-27 06:45:18 -0700

    Queue text animations

--- TextAnimation.gd
+++ TextAnimation.gd
@@ -2,48 +2,55 @@ class_name TextAnimation extends Label
 
 @onready var animationPlayer := $AnimationPlayer
 
+var data = []
+
+func _ready():
+    animationPlayer.animation_changed.connect(switch_data)
+
+func switch_data(_old, _new):
+    var color_and_text = data.pop_front()
+    set_color(color_and_text[0])
+    text = color_and_text[1]
+    return
+
+func queue_animation(c, t):
+    data.append([c, t])    
+    if animationPlayer.is_playing():
+        animationPlayer.queue("Float")
+    else:
+        switch_data("", "")
+        animationPlayer.play("Float")
+    return
 
 func lose_health(h: int):
-    set_color(Color.RED)
-    text =  "-" + str(h) + " Health"
-    play()
+    queue_animation(Color.RED, "-" + str(h) + " Health")
     return
 
 func gain_health(h: int):
-    set_color(Color.GREEN)
-    text = "+" + str(h) + " Health"
-    play()
+    queue_animation(Color.GREEN, "+" + str(h) + " Health")
     return
 
 func update_energy(e: int):
-    set_color(Color.CYAN)
+    var t = ""
     if e < 0:
-        text = "-" + str(e) + " Energy"
+        t = "-" + str(e) + " Energy"
     else:
-        text = "+" + str(e) + " Energy"
-    play()
+        t = "+" + str(e) + " Energy"
+    queue_animation(Color.CYAN, t)
     return
 
 func gain_shields(s: int):
-    set_color(Color.CYAN)
-    text = "+" + str(s) + " Shield"
-    play()
+    queue_animation(Color.CYAN, "+" + str(s) + " Shield")
     return
 
 func gain_movement(m: int):
-    set_color(Color.GOLD)
-    text = "+" + str(m) + " Movement"
-    play()
-    return
-
-func play():
-    animationPlayer.play("Float")    
+    queue_animation(Color.GOLD, "+" + str(m) + " Movement")
     return
 
 func set_color(c: Color):
     var ani : Animation = animationPlayer.get_animation("Float")
     var idx = ani.find_track(^".:theme_override_colors/font_color", Animation.TYPE_VALUE)
-    var k := ani.track_find_key(idx, 1)
+    var k := ani.track_find_key(idx, .55)
     ani.track_set_key_value(idx, k, c)
     return
     
