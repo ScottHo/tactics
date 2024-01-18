889fb5b258a383ef1ef311dc30582159d0b5d5b0
Scott Ho <sscott.ho@gmail.com>
2024-01-18 00:20:44 -0700

    Scale the tile map, add new tiles

--- Highlights.gd
+++ Highlights.gd
@@ -1,8 +1,12 @@
 class_name Highlights
 
 static var EMPTY = Vector2i(0,0)
-static var RED = Vector2i(1,0)
+static var SELECTED = Vector2i(1,0)
 static var GREEN = Vector2i(2,0)
 static var YELLOW = Vector2i(3,0)
-static var BLUE = Vector2i(0,1)
-static var PURPLE = Vector2i(1,1)
+static var PURPLE = Vector2i(4,0)
+static var CYAN = Vector2i(5,0)
+static var GREY = Vector2i(1,0)
+static var ORANGE = Vector2i(1,1)
+static var BLUE = Vector2i(1,2)
+static var RED = Vector2i(1,3)
