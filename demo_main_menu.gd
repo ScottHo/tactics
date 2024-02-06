ebb15fd70573d2ac820e461cd74683693b873f96
Scott Ho <sscott.ho@gmail.com>
2024-02-06 00:53:06 -0700

    Large text updates

--- demo_main_menu.gd
+++ demo_main_menu.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
 func _ready():
-    $Buster/AnimationPlayer.play("Move")
-    $Buster/CharacterCommon/InfoBoxContainer.visible = false
-    $Label2.visible = false    
-    if Globals.current_level > 8:
-        $Control/Control/Button.disabled = true
-        $Label2.visible = true
     $Control/Control/Button.pressed.connect(func():
         get_tree().change_scene_to_file("res://prologue.tscn")
     )
